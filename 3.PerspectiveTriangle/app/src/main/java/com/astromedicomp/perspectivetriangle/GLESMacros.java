53b7eebb1df658df03b90ff0bcb5d86cf482cb44
zodgevaibhav <zodgevaibhav@gmail.com>
2018-01-29 17:44:04 +0530

    Perspective triange

---
+++ 3.PerspectiveTriangle/app/src/main/java/com/astromedicomp/perspectivetriangle/GLESMacros.java
@@ -0,0 +1,9 @@
+package com.astromedicomp.perspectivetriangle;
+
+public class GLESMacros
+{
+	public static final int VVZ_ATTRIB_VERTEX=0;
+	public static final int VVZ_ATTRIB_COLOR=1;
+	public static final int VVZ_ATTRIB_NORMAL=2;
+	public static final int VVZ_ATTRIB_TEXTURE=3;
+}
