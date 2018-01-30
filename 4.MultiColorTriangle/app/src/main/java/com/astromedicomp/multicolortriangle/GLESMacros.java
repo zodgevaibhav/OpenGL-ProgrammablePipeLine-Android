9d91ce89051a05b32699080733ce0d0321094b60
zodgevaibhav <zodgevaibhav@gmail.com>
2018-01-30 18:13:29 +0530

    Multi Color Triangle - Not working

---
+++ 4.MultiColorTriangle/app/src/main/java/com/astromedicomp/multicolortriangle/GLESMacros.java
@@ -0,0 +1,9 @@
+package com.astromedicomp.multicolortriangle;
+
+public class GLESMacros
+{
+	public static final int VVZ_ATTRIB_VERTEX=0;
+	public static final int VVZ_ATTRIB_COLOR=1;
+	public static final int VVZ_ATTRIB_NORMAL=2;
+	public static final int VVZ_ATTRIB_TEXTURE=3;
+}
